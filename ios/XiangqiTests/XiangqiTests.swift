3809901bbbf93ed5c5155a88751271cf180d047b
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-02 19:32:25 +0800

    Initialize iOS SpriteKit project.

---
+++ ios/XiangqiTests/XiangqiTests.swift
@@ -0,0 +1,17 @@
+//
+//  XiangqiTests.swift
+//  XiangqiTests
+//
+//  Created by Shuyang Sun on 2/2/25.
+//
+
+import Testing
+@testable import Xiangqi
+
+struct XiangqiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
