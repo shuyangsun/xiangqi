5398deb54d54c52336be1c3ad969c83573e2d52f
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-17 10:30:06 -0500

    Add possible boards debug.

--- benchmarks/bench_possible_moves.cc
+++ benchmarks/bench_possible_moves.cc
@@ -1,16 +1,30 @@
 #include <benchmark/benchmark.h>
 
+#include <array>
+
+#include "xiangqi/board.h"
+#include "xiangqi/board_c.h"
+
 namespace {
+
+namespace {
+
+using ::xq::Board;
+using ::xq::kStartingBoard;
+using ::xq::PossibleBoards;
+
+}  // namespace
 
 static void BM_PossibleBoards(benchmark::State& state) {
   for (auto _ : state) {
-    // TODO
+    PossibleBoards(kStartingBoard, PLAYER_RED, false);
   }
 }
 
 static void BM_PossibleBoards_C(benchmark::State& state) {
+  std::array<Piece, K_BOARD_SIZE * K_MAX_MOVE_PER_PLAYER> out;
   for (auto _ : state) {
-    // TODO
+    PossibleBoards_C(K_STARTING_BOARD, PLAYER_RED, false, out.data());
   }
 }
 
