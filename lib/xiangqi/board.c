ca266b6b501859a3bf760a18cc709da206a96dd6
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-16 20:00:22 -0500

    Move single piece possible moves implementation to C.

--- lib/xiangqi/board.c
+++ lib/xiangqi/board.c
@@ -6,6 +6,8 @@
 // --------------- Helper Function ---------------
 
 #define CAN_CAPTURE(a, b) ((b) ? ((a) <= 0) : ((a) >= 0))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 static inline bool IsPathClear(const BoardC board, const Position from,
                                const Position to) {
@@ -116,6 +118,517 @@ static inline bool ThreatensByCannon(const BoardC board, const Position pos,
     return found_in_between;
   }
   return false;
+}
+
+static inline uint8_t PossibleMovesGeneral(const BoardC board,
+                                           const Position pos,
+                                           const Position opponent_general,
+                                           Position* out) {
+  uint8_t res = 0;
+
+  // Flying general check.
+  const enum Piece piece = board[pos];
+  bool is_blocked = false;
+  if (opponent_general != K_NO_POSITION && Col(opponent_general) == Col(pos)) {
+    for (Position cur = MIN(pos, opponent_general) + K_TOTAL_COL;
+         cur < MAX(pos, opponent_general); cur += K_TOTAL_COL) {
+      if (!IsEmpty(board[cur])) {
+        is_blocked = true;
+        break;
+      }
+    }
+    if (!is_blocked) {
+      *(out + res++) = opponent_general;
+    }
+  }
+
+  if (piece == R_GENERAL) {
+    if (((pos >= 66 && pos <= 68) || (pos >= 75 && pos <= 77)) &&
+        !IsRed(board[pos + K_TOTAL_COL])) {
+      // move down
+      *(out + res++) = pos + K_TOTAL_COL;
+    }
+    if (((pos >= 75 && pos <= 77) || (pos >= 84 && pos <= 86)) &&
+        !IsRed(board[pos - K_TOTAL_COL])) {
+      // move up
+      *(out + res++) = pos - K_TOTAL_COL;
+    }
+    if ((pos == 66 || pos == 67 || pos == 75 || pos == 76 || pos == 84 ||
+         pos == 85) &&
+        !IsRed(board[pos + 1])) {
+      // move right
+      *(out + res++) = pos + 1;
+    }
+    if ((pos == 67 || pos == 68 || pos == 76 || pos == 77 || pos == 85 ||
+         pos == 86) &&
+        !IsRed(board[pos - 1])) {
+      // move left
+      *(out + res++) = pos - 1;
+    }
+  } else if (piece == B_GENERAL) {
+    if (((pos >= 3 && pos <= 5) || (pos >= 12 && pos <= 14)) &&
+        !IsBlack(board[pos + K_TOTAL_COL])) {
+      // move down
+      *(out + res++) = pos + K_TOTAL_COL;
+    }
+    if (((pos >= 12 && pos <= 14) || (pos >= 21 && pos <= 23)) &&
+        !IsBlack(board[pos - K_TOTAL_COL])) {
+      // move up
+      *(out + res++) = pos - K_TOTAL_COL;
+    }
+    if ((pos == 3 || pos == 4 || pos == 12 || pos == 13 || pos == 21 ||
+         pos == 22) &&
+        !IsBlack(board[pos + 1])) {
+      // move right
+      *(out + res++) = pos + 1;
+    }
+    if ((pos == 4 || pos == 5 || pos == 13 || pos == 14 || pos == 22 ||
+         pos == 23) &&
+        !IsBlack(board[pos - 1])) {
+      // move left
+      *(out + res++) = pos - 1;
+    }
+  }
+  return res;
+}
+
+static inline uint8_t PossibleMovesAdvisor(const BoardC board,
+                                           const Position pos, Position* out) {
+  uint8_t res = 0;
+
+  if (IsRed(board[pos])) {
+    switch (pos) {
+      case 84:  // D9
+      case 86:  // F9
+      case 66:  // D7
+      case 68:  // F7
+        if (!IsRed(board[76])) {
+          *(out + res++) = 76;  // E8
+        }
+        break;
+      case 76:  // E8
+        if (!IsRed(board[84])) {
+          *(out + res++) = 84;
+        }
+        if (!IsRed(board[86])) {
+          *(out + res++) = 86;
+        }
+        if (!IsRed(board[66])) {
+          *(out + res++) = 66;
+        }
+        if (!IsRed(board[68])) {
+          *(out + res++) = 68;
+        }
+        break;
+    }
+  } else {
+    switch (pos) {
+      case 3:   // D0
+      case 5:   // F0
+      case 21:  // D2
+      case 23:  // F2
+        if (!IsBlack(board[13])) {
+          *(out + res++) = 13;  // E1
+        }
+        break;
+      case 13:  // E1
+        if (!IsBlack(board[3])) {
+          *(out + res++) = 3;
+        }
+        if (!IsBlack(board[5])) {
+          *(out + res++) = 5;
+        }
+        if (!IsBlack(board[21])) {
+          *(out + res++) = 21;
+        }
+        if (!IsBlack(board[23])) {
+          *(out + res++) = 23;
+        }
+        break;
+    }
+  }
+
+  return res;
+}
+
+static inline uint8_t PossibleMovesElephant(const BoardC board,
+                                            const Position pos, Position* out) {
+  uint8_t res = 0;
+
+  if (IsRed(board[pos])) {
+    switch (pos) {
+      case 83:  // C9
+        if (!IsRed(board[63]) && IsEmpty(board[73])) {
+          *(out + res++) = 63;
+        }
+        if (!IsRed(board[67]) && IsEmpty(board[75])) {
+          *(out + res++) = 67;
+        }
+        break;
+      case 87:  // G9
+        if (!IsRed(board[71]) && IsEmpty(board[79])) {
+          *(out + res++) = 71;
+        }
+        if (!IsRed(board[67]) && IsEmpty(board[77])) {
+          *(out + res++) = 67;
+        }
+        break;
+      case 63:  // A7
+        if (!IsRed(board[47]) && IsEmpty(board[55])) {
+          *(out + res++) = 47;
+        }
+        if (!IsRed(board[83]) && IsEmpty(board[73])) {
+          *(out + res++) = 83;
+        }
+        break;
+      case 71:  // I7
+        if (!IsRed(board[51]) && IsEmpty(board[61])) {
+          *(out + res++) = 51;
+        }
+        if (!IsRed(board[87]) && IsEmpty(board[79])) {
+          *(out + res++) = 87;
+        }
+        break;
+      case 47:  // C5
+        if (!IsRed(board[63]) && IsEmpty(board[55])) {
+          *(out + res++) = 63;
+        }
+        if (!IsRed(board[67]) && IsEmpty(board[57])) {
+          *(out + res++) = 67;
+        }
+        break;
+      case 51:  // G5
+        if (!IsRed(board[71]) && IsEmpty(board[61])) {
+          *(out + res++) = 71;
+        }
+        if (!IsRed(board[67]) && IsEmpty(board[59])) {
+          *(out + res++) = 67;
+        }
+        break;
+      case 67:  // E7
+        if (!IsRed(board[47]) && IsEmpty(board[57])) {
+          *(out + res++) = 47;
+        }
+        if (!IsRed(board[83]) && IsEmpty(board[75])) {
+          *(out + res++) = 83;
+        }
+        if (!IsRed(board[51]) && IsEmpty(board[59])) {
+          *(out + res++) = 51;
+        }
+        if (!IsRed(board[87]) && IsEmpty(board[77])) {
+          *(out + res++) = 87;
+        }
+        break;
+    }
+  } else {
+    switch (pos) {
+      case 2:  // C0
+        if (!IsBlack(board[18]) && IsEmpty(board[10])) {
+          *(out + res++) = 18;
+        }
+        if (!IsBlack(board[22]) && IsEmpty(board[12])) {
+          *(out + res++) = 22;
+        }
+        break;
+      case 6:  // G0
+        if (!IsBlack(board[26]) && IsEmpty(board[16])) {
+          *(out + res++) = 26;
+        }
+        if (!IsBlack(board[22]) && IsEmpty(board[14])) {
+          *(out + res++) = 22;
+        }
+        break;
+      case 18:  // A2
+        if (!IsBlack(board[2]) && IsEmpty(board[10])) {
+          *(out + res++) = 2;
+        }
+        if (!IsBlack(board[38]) && IsEmpty(board[28])) {
+          *(out + res++) = 38;
+        }
+        break;
+      case 26:  // I2
+        if (!IsBlack(board[6]) && IsEmpty(board[16])) {
+          *(out + res++) = 6;
+        }
+        if (!IsBlack(board[42]) && IsEmpty(board[34])) {
+          *(out + res++) = 42;
+        }
+        break;
+      case 38:  // C4
+        if (!IsBlack(board[18]) && IsEmpty(board[28])) {
+          *(out + res++) = 18;
+        }
+        if (!IsBlack(board[22]) && IsEmpty(board[30])) {
+          *(out + res++) = 22;
+        }
+        break;
+      case 42:  // G4
+        if (!IsBlack(board[26]) && IsEmpty(board[34])) {
+          *(out + res++) = 26;
+        }
+        if (!IsBlack(board[22]) && IsEmpty(board[32])) {
+          *(out + res++) = 22;
+        }
+        break;
+      case 22:  // E2
+        if (!IsBlack(board[2]) && IsEmpty(board[12])) {
+          *(out + res++) = 2;
+        }
+        if (!IsBlack(board[6]) && IsEmpty(board[14])) {
+          *(out + res++) = 6;
+        }
+        if (!IsBlack(board[38]) && IsEmpty(board[30])) {
+          *(out + res++) = 38;
+        }
+        if (!IsBlack(board[42]) && IsEmpty(board[32])) {
+          *(out + res++) = 42;
+        }
+        break;
+    }
+  }
+  return res;
+}
+
+static inline uint8_t PossibleMovesHorse(const BoardC board, const Position pos,
+                                         Position* out) {
+  const bool is_red = IsRed(board[pos]);
+  const uint8_t col = Col(pos);
+  uint8_t res = 0;
+
+  if (pos > 17) {                             // can move up 2
+    if (IsEmpty(board[pos - K_TOTAL_COL])) {  // not blocked
+      const Position up = pos - 2 * K_TOTAL_COL;
+      if (col > 0 && CAN_CAPTURE(board[up - 1], is_red)) {
+        *(out + res++) = up - 1;
+      }
+      if (col < K_TOTAL_COL - 1 && CAN_CAPTURE(board[up + 1], is_red)) {
+        *(out + res++) = up + 1;
+      }
+    }
+  }
+  if (pos < 72) {                             // can move down 2
+    if (IsEmpty(board[pos + K_TOTAL_COL])) {  // not blocked
+      const Position down = pos + 2 * K_TOTAL_COL;
+      if (col > 0 && CAN_CAPTURE(board[down - 1], is_red)) {
+        *(out + res++) = down - 1;
+      }
+      if (col < K_TOTAL_COL - 1 && CAN_CAPTURE(board[down + 1], is_red)) {
+        *(out + res++) = down + 1;
+      }
+    }
+  }
+  if (col > 1) {                    // can move left 2
+    if (IsEmpty(board[pos - 1])) {  // not blocked
+      const Position left = pos - 2;
+      if (pos > 8 && CAN_CAPTURE(board[left - K_TOTAL_COL], is_red)) {
+        *(out + res++) = left - K_TOTAL_COL;
+      }
+      if (pos < 81 && CAN_CAPTURE(board[left + K_TOTAL_COL], is_red)) {
+        *(out + res++) = left + K_TOTAL_COL;
+      }
+    }
+  }
+  if (col < K_TOTAL_COL - 2) {      // can move right 2
+    if (IsEmpty(board[pos + 1])) {  // not blocked
+      const Position right = pos + 2;
+      if (pos > 8 && CAN_CAPTURE(board[right - K_TOTAL_COL], is_red)) {
+        *(out + res++) = right - K_TOTAL_COL;
+      }
+      if (pos < 81 && CAN_CAPTURE(board[right + K_TOTAL_COL], is_red)) {
+        *(out + res++) = right + K_TOTAL_COL;
+      }
+    }
+  }
+
+  return res;
+}
+
+static inline uint8_t PossibleMovesChariot(const BoardC board,
+                                           const Position pos, Position* out) {
+  const bool is_red = IsRed(board[pos]);
+  uint8_t res = 0;
+
+  // Up
+  for (int8_t cur = ((int8_t)pos) - K_TOTAL_COL; cur >= 0; cur -= K_TOTAL_COL) {
+    if (CAN_CAPTURE(board[cur], is_red)) {
+      *(out + res++) = cur;
+      if (!IsEmpty(board[cur])) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  // Down
+  for (int8_t cur = pos + K_TOTAL_COL; cur < K_BOARD_SIZE; cur += K_TOTAL_COL) {
+    if (CAN_CAPTURE(board[cur], is_red)) {
+      *(out + res++) = cur;
+      if (!IsEmpty(board[cur])) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  // Left
+  for (int8_t cur = ((int8_t)pos) - 1; cur >= (pos - pos % K_TOTAL_COL);
+       cur--) {
+    if (CAN_CAPTURE(board[cur], is_red)) {
+      *(out + res++) = cur;
+      if (!IsEmpty(board[cur])) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  // Right
+  for (int8_t cur = pos + 1; cur < ((pos / K_TOTAL_COL + 1) * K_TOTAL_COL);
+       cur++) {
+    if (CAN_CAPTURE(board[cur], is_red)) {
+      *(out + res++) = cur;
+      if (!IsEmpty(board[cur])) {
+        break;
+      }
+    } else {
+      break;
+    }
+  }
+
+  return res;
+}
+
+static inline uint8_t PossibleMovesCannon(const BoardC board,
+                                          const Position pos, Position* out) {
+  const bool is_red = IsRed(board[pos]);
+  uint8_t res = 0;
+
+  // Up
+  bool blocked = false;
+  for (int8_t cur = ((int8_t)pos) - K_TOTAL_COL; cur >= 0; cur -= K_TOTAL_COL) {
+    if (IsEmpty(board[cur])) {
+      if (!blocked) {
+        *(out + res++) = cur;
+      }
+    } else {
+      if (blocked) {
+        if (CAN_CAPTURE(board[cur], is_red)) {
+          *(out + res++) = cur;
+        }
+        break;
+      } else {
+        blocked = true;
+      }
+    }
+  }
+
+  // Down
+  blocked = false;
+  for (int8_t cur = pos + K_TOTAL_COL; cur < K_BOARD_SIZE; cur += K_TOTAL_COL) {
+    if (IsEmpty(board[cur])) {
+      if (!blocked) {
+        *(out + res++) = cur;
+      }
+    } else {
+      if (blocked) {
+        if (CAN_CAPTURE(board[cur], is_red)) {
+          *(out + res++) = cur;
+        }
+        break;
+      } else {
+        blocked = true;
+      }
+    }
+  }
+
+  // Left
+  blocked = false;
+  for (int8_t cur = ((int8_t)pos) - 1; cur >= (pos - pos % K_TOTAL_COL);
+       cur--) {
+    if (IsEmpty(board[cur])) {
+      if (!blocked) {
+        *(out + res++) = cur;
+      }
+    } else {
+      if (blocked) {
+        if (CAN_CAPTURE(board[cur], is_red)) {
+          *(out + res++) = cur;
+        }
+        break;
+      } else {
+        blocked = true;
+      }
+    }
+  }
+
+  // Right
+  blocked = false;
+  for (int8_t cur = pos + 1; cur < ((pos / K_TOTAL_COL + 1) * K_TOTAL_COL);
+       cur++) {
+    if (IsEmpty(board[cur])) {
+      if (!blocked) {
+        *(out + res++) = cur;
+      }
+    } else {
+      if (blocked) {
+        if (CAN_CAPTURE(board[cur], is_red)) {
+          *(out + res++) = cur;
+        }
+        break;
+      } else {
+        blocked = true;
+      }
+    }
+  }
+
+  return res;
+}
+
+static inline uint8_t PossibleMovesSoldier(const BoardC board,
+                                           const Position pos, Position* out) {
+  const bool is_red = IsRed(board[pos]);
+  uint8_t res = 0;
+
+  if (is_red) {
+    if (pos >= K_RED_RIVER_START &&
+        CAN_CAPTURE(board[pos - K_TOTAL_COL], is_red)) {
+      *out = pos - K_TOTAL_COL;
+      return 1;
+    } else {
+      if (pos >= K_TOTAL_COL && CAN_CAPTURE(board[pos - K_TOTAL_COL], is_red)) {
+        *(out + res++) = pos - K_TOTAL_COL;
+      }
+      const uint8_t col = Col(pos);
+      if (col > 0 && CAN_CAPTURE(board[pos - 1], is_red)) {
+        *(out + res++) = pos - 1;
+      }
+      if (col < K_TOTAL_COL - 1 && CAN_CAPTURE(board[pos + 1], is_red)) {
+        *(out + res++) = pos + 1;
+      }
+      return res;
+    }
+  } else {
+    if (pos < K_RED_RIVER_START &&
+        CAN_CAPTURE(board[pos + K_TOTAL_COL], is_red)) {
+      *out = pos + K_TOTAL_COL;
+      return 1;
+    } else {
+      if (pos < 81 && CAN_CAPTURE(board[pos + K_TOTAL_COL], is_red)) {
+        *(out + res++) = pos + K_TOTAL_COL;
+      }
+      const uint8_t col = Col(pos);
+      if (col > 0 && CAN_CAPTURE(board[pos - 1], is_red)) {
+        *(out + res++) = pos - 1;
+      }
+      if (col < K_TOTAL_COL - 1 && CAN_CAPTURE(board[pos + 1], is_red)) {
+        *(out + res++) = pos + 1;
+      }
+      return res;
+    }
+  }
 }
 
 // --------------- Public Function ---------------
@@ -635,7 +1148,70 @@ void DecodeBoardState_C(const BoardStateC state, BoardC out) {
 
 uint8_t PossibleMoves_C(const BoardC board, const Position pos,
                         const bool avoid_checkmate, MovesPerPieceC out) {
-  return 0;  // TODO
+  memset(out, 0xFF, 17);
+  uint8_t res = 0;
+
+  const enum Piece piece = board[pos];
+  switch (piece) {
+    case PIECE_EMPTY:
+      return res;
+    case R_GENERAL:
+      res = PossibleMovesGeneral(board, pos, FindGeneral_C(board, PLAYER_BLACK),
+                                 out);
+      break;
+    case B_GENERAL:
+      res = PossibleMovesGeneral(board, pos, FindGeneral_C(board, PLAYER_RED),
+                                 out);
+      break;
+    case R_ADVISOR:
+    case B_ADVISOR:
+      res = PossibleMovesAdvisor(board, pos, out);
+      break;
+    case R_ELEPHANT:
+    case B_ELEPHANT:
+      res = PossibleMovesElephant(board, pos, out);
+      break;
+    case R_HORSE:
+    case B_HORSE:
+      res = PossibleMovesHorse(board, pos, out);
+      break;
+    case R_CHARIOT:
+    case B_CHARIOT:
+      res = PossibleMovesChariot(board, pos, out);
+      break;
+    case R_CANNON:
+    case B_CANNON:
+      res = PossibleMovesCannon(board, pos, out);
+      break;
+    case R_SOLDIER:
+    case B_SOLDIER:
+      res = PossibleMovesSoldier(board, pos, out);
+      break;
+    default:
+      return res;
+  }
+  if (avoid_checkmate) {
+    const enum Player player = IsRed(piece) ? PLAYER_RED : PLAYER_BLACK;
+    for (size_t i = 0; i < res && *(out + i) != K_NO_POSITION; i++) {
+      BoardC next;
+      CopyBoard_C(next, board);
+      const enum Piece captured = Move_C(next, NewMovement(pos, *(out + i)));
+      if (captured == R_GENERAL || captured == B_GENERAL) {
+        continue;
+      }
+      if (IsBeingCheckmate_C(next, player)) {
+        // Replace the current result with the last one, and set the last one
+        // to no position.
+        *(out + i) = *(out + res - 1);
+        *(out + res - 1) = K_NO_POSITION;
+        res--;
+        i--;
+      }
+    }
+  }
+  return res;
 }
 
 #undef CAN_CAPTURE
+#undef MIN
+#undef MAX
