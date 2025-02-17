a9516fe55292debc353e155d99e73daeb7036b44
Shuyang Sun <shuyangsun10@gmail.com>
2025-02-17 10:45:11 -0500

    More benchmarks.

--- benchmarks/bench_possible_moves.cc
+++ benchmarks/bench_possible_moves.cc
@@ -12,12 +12,33 @@ namespace {
 using ::xq::Board;
 using ::xq::kStartingBoard;
 using ::xq::PossibleBoards;
+using ::xq::PossibleMoves;
 
 }  // namespace
 
-static void BM_PossibleBoards(benchmark::State& state) {
+static void BM_PossibleMoves_C(benchmark::State& state) {
+  MaxMovesPerPlayerC out;
   for (auto _ : state) {
-    PossibleBoards(kStartingBoard, PLAYER_RED, false);
+    PossibleMoves_C(K_STARTING_BOARD, PLAYER_RED, false, out);
+  }
+}
+
+static void BM_PossibleMoves_C_AvoidCheckmate(benchmark::State& state) {
+  MaxMovesPerPlayerC out;
+  for (auto _ : state) {
+    PossibleMoves_C(K_STARTING_BOARD, PLAYER_RED, true, out);
+  }
+}
+
+static void BM_PossibleMoves(benchmark::State& state) {
+  for (auto _ : state) {
+    PossibleMoves(kStartingBoard, PLAYER_RED, false);
+  }
+}
+
+static void BM_PossibleMoves_AvoidCheckmate(benchmark::State& state) {
+  for (auto _ : state) {
+    PossibleMoves(kStartingBoard, PLAYER_RED, true);
   }
 }
 
@@ -28,8 +49,34 @@ static void BM_PossibleBoards_C(benchmark::State& state) {
   }
 }
 
-BENCHMARK(BM_PossibleBoards);
+static void BM_PossibleBoards_C_AvoidCheckmate(benchmark::State& state) {
+  std::array<Piece, K_BOARD_SIZE * K_MAX_MOVE_PER_PLAYER> out;
+  for (auto _ : state) {
+    PossibleBoards_C(K_STARTING_BOARD, PLAYER_RED, true, out.data());
+  }
+}
+
+static void BM_PossibleBoards(benchmark::State& state) {
+  for (auto _ : state) {
+    PossibleBoards(kStartingBoard, PLAYER_RED, false);
+  }
+}
+
+static void BM_PossibleBoards_AvoidCheckmate(benchmark::State& state) {
+  for (auto _ : state) {
+    PossibleBoards(kStartingBoard, PLAYER_RED, true);
+  }
+}
+
+BENCHMARK(BM_PossibleMoves_C);
+BENCHMARK(BM_PossibleMoves_C_AvoidCheckmate);
+BENCHMARK(BM_PossibleMoves);
+BENCHMARK(BM_PossibleMoves_AvoidCheckmate);
+
 BENCHMARK(BM_PossibleBoards_C);
+BENCHMARK(BM_PossibleBoards_C_AvoidCheckmate);
+BENCHMARK(BM_PossibleBoards);
+BENCHMARK(BM_PossibleBoards_AvoidCheckmate);
 
 }  // namespace
 
